f3251bd9141392e7aabbe975b0d5595a823c7d46
gwbaik9717 <gwbaik9717@icloud.com>
2022-01-30 11:59:26 +0900

    mystery solved
    - 객체를 로깅할 때는 객체의 내용 변경사항이 실시간으로 업데이트 된다.
    - splice 반환값은 '삭제된 배열'이다.

--- 4-3.js
+++ 4-3.js
@@ -1,37 +1,33 @@
-function solution(arr){
+function solution(arr) {
     let ans = [];
-    let tchr = [];
-    let stnt = [];
-    
-    for(let i=0; i<arr[0].length-1; i++){
-      for(let j=i+1; j<arr[0].length; j++){
-        tchr.push(arr[0][i]);
-        stnt.push(arr[0][j]);
-      }
-    }
   
-    for(let i=1; i<arr.length; i++){
-      for(let j=0; j<tchr.length; j++){
-        let left = tchr[j];
-        let right = stnt[j];
-  
-        if(arr[i].indexOf(left) > arr[i].indexOf(right)){
-          tchr[j] = null;
-          stnt[j] = null;
+    for (let i = 0; i < arr[0].length - 1; i++) {
+        for (let j = i + 1; j < arr[0].length; j++) {
+            const temp = [arr[0][i], arr[0][j]];
+            ans.push(temp);
         }
-      }
+    }  
+  
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = 0; j < ans.length; j++) {
+            let tchr = ans[j][0];
+            let stnt = ans[j][1];
+  
+            if (arr[i].indexOf(tchr) > arr[i].indexOf(stnt)) {
+                ans.splice(j, 1);
+                j--;
+            }
+        }
     }
   
-    let count = 0;
+    console.log(ans);
   
-    for(let x of stnt){
-      if(!x){
-        count++;
-      }
-    }
-  
-    return count;
+    return ans.length;
   }
   
-  let arr=[[3, 4, 1, 2], [4, 3, 2, 1], [3, 1, 4, 2]];
+  let arr = [
+    [3, 4, 1, 2],
+    [4, 3, 2, 1],
+    [3, 1, 4, 2]
+  ];
   console.log(solution(arr));
