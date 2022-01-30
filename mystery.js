c1138453d137332b7e3e019d3a636f3af2ad5007
gwbaik9717 <gwbaik9717@icloud.com>
2022-01-30 10:58:47 +0900

    fixing..

---
+++ mystery.js
@@ -0,0 +1,34 @@
+function solution(arr){
+    let ans = [];
+    
+    for(let i=0; i<arr[0].length-1; i++){
+      for(let j=i+1; j<arr[0].length; j++){
+        const temp = [arr[0][i], arr[0][j]];
+        console.log(temp);
+        
+        ans.push(temp);
+        console.log(ans)
+      }
+    }
+  
+    console.log(ans);
+  
+  
+    for(let i=1; i<arr.length; i++){
+      for(let j=0; j<ans.length; j++){
+        let tchr = ans[j][0];
+        let stnt = ans[j][1];
+  
+        if(arr[i].indexOf(tchr) > arr[i].indexOf(stnt)){
+          ans[j] = null;
+        } 
+      }
+    }
+    
+    console.log(ans);
+               
+    return ans.length;
+  }
+  
+  let arr=[[3, 4, 1, 2], [4, 3, 2, 1], [3, 1, 4, 2]];
+  console.log(solution(arr));
